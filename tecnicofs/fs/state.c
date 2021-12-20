8fb2ac5a4f2e74e1d3d1d5d1f6a4557b532d049b
maumia <manuelmldias@gmail.com>
2021-12-20 18:23:25 +0000

    a

--- tecnicofs/fs/state.c
+++ tecnicofs/fs/state.c
@@ -90,7 +90,7 @@ void state_destroy() { /* nothing to do */
  */
 int inode_create(inode_type n_type) {
     for (int inumber = 0; inumber < INODE_TABLE_SIZE; inumber++) {
-        if ((inumber * (int) sizeof(allocation_state_t)) == 0) {
+        if ((inumber * (int) sizeof(allocation_state_t) % BLOCK_SIZE) == 0){
             insert_delay(); // simulate storage access delay (to freeinode_ts)
         }
 
