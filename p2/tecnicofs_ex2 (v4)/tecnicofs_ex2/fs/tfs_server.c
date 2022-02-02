012d4e361a4892a6ce7d997a42b827ad44e8c2b2
maumia <manuelmldias@gmail.com>
2022-02-02 17:56:51 +0000

    Update tfs_server.c

--- p2/tecnicofs_ex2 (v4)/tecnicofs_ex2/fs/tfs_server.c
+++ p2/tecnicofs_ex2 (v4)/tecnicofs_ex2/fs/tfs_server.c
@@ -26,6 +26,7 @@ int ses(){
 
 void tfs_sv_mount(){
     int id;
+    read(svfileopen, &id,sizeof(int));
     void* mbuffer = (void*) malloc(sizeof(char)*40);
     read(svfileopen, mbuffer, 40);
     int fcl = open(mbuffer, O_WRONLY);
@@ -58,21 +59,78 @@ void tfs_sv_unmount(){
 
     int id;
     read(svfileopen, &id,sizeof(int));
+    int fcl = open(mbuffer, O_WRONLY);
+    if (fcl != 1){
+
+        printf("Error opening");
+        exit(2);
+
+    }
     int fcl = ses_id[id];
     close(fcl);
 }
 
-void tfs_sv_open(char const *name, int flags){
+void tfs_sv_open(){
 
     int id;
+    char const* name;
+    int flags;
+    
+    void* mbuffer = (void*) malloc(sizeof(char)*40);
     read(svfileopen, &id, sizeof(int));
-    int fcl = ses_id[id];
-    tfs_open(name,flags);
+    read(svfileopen + sizeof(int), &name, sizeof(char)*40);
+    read(svfileopen + sizeof(char) * 40 + sizeof(int), &flags, sizeof(int));
+    int fcl = open(mbuffer, O_WRONLY);
+    if (fcl != 1){
 
+        printf("Error opening");
+        exit(2);
+
+    }
+    int fcl = ses_id[id];
+    
+    tfs_open(&name , flags);
+
+}
+
+void tfs_sv_close(){
+
+    int id;
+    int fhandle;
+
+    read(svfileopen, &id, sizeof(int));
+    read(svfileopen + sizeof(int), &fhandle, sizeof(int));
+    int fcl = open(mbuffer, O_WRONLY);
+    if (fcl != 1){
+
+        printf("Error opening");
+        exit(2);
+
+    }
+    int fcl = ses_id[id];
+
+    tfs_close(fhandle);
 
 
 }
 
+void tfs_sv_write(){
+
+    int id;
+    int fhandle;
+    size_t len;
+    char[len] buff_cont;
+
+    read(svfileopen, &id, sizeof(int));
+    read(svfileopen + sizeof(int), &fhandle, sizeof(int));
+    read(svfileopen + (sizeof(int) * 2), &len, sizeof(int));
+    read(svfileopen + (sizeof(int) * 3), &buff_cont, sizeof(int));
+
+
+
+
+
+}
 
 int main(int argc, char **argv) {
 
@@ -113,16 +171,19 @@ int main(int argc, char **argv) {
             break;
         
         case TFS_OP_CODE_UNMOUNT :
-
+            tfs_sv_unmount();
             break;
 
         case TFS_OP_CODE_OPEN : 
+            tfs_sv_open();
             break;
         
         case TFS_OP_CODE_CLOSE :
+            tfs_sv_close();
             break;
 
         case TFS_OP_CODE_WRITE : 
+            tfs_sv_write();
             break;
         
         case TFS_OP_CODE_READ :
